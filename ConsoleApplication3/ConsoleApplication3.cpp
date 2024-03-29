c69280a5c421af88c0fefdc2f9f95840c2d25fc9
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-03-29 14:33:16 +0700

    Menambahkan else

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -8,9 +8,12 @@ int n;
 
 void input() {
 	while (true)
-}
-	cout << "Masukan Panjang elemen array: ";
-	cin >> n;
+	{
+		cout << "Masukan Panjang elemen array: ";
+		cin >> n;
 
-	if (n <= 20)
-		break;
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimal panjang array adalah 20" << endl;
+	}
